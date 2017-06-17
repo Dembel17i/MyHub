85c9052fe7afafbc78245e5c2a556930e21025f1
Dembel17i <Dembel17i@gmail.com>
2017-06-17 12:59:16 +0300

    Добавил метод hello

--- GitTrain/src/MainClass.java
+++ GitTrain/src/MainClass.java
@@ -1,7 +1,11 @@
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello Git!11");
+        System.out.println(hello("Hello Git!11"));
+    }
+
+    static String hello(String str) {
+        return str;
     }
 
 }
