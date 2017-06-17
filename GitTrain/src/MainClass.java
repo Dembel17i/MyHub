71232ff6e8264f3555b504df384e972304a03710
Dembel17i <Dembel17i@gmail.com>
2017-06-17 12:36:08 +0300

    Добавил метод add

--- GitTrain/src/MainClass.java
+++ GitTrain/src/MainClass.java
@@ -1,7 +1,14 @@
 
 public class MainClass {
     public static void main(String[] args) {
+
         System.out.println("Hello Git!11");
+        System.out.println(add(2, 8));
     }
 
-}
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+
+}//MainClass
