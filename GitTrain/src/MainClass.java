582bcfd5e4a58e6a40f1e7a48fc7352ae69fd22f
Dembel17i <Dembel17i@gmail.com>
2017-06-17 12:44:42 +0300

    Добавил метод sub

--- GitTrain/src/MainClass.java
+++ GitTrain/src/MainClass.java
@@ -4,10 +4,16 @@ public class MainClass {
 
         System.out.println("Hello Git!11");
         System.out.println(add(2, 8));
+        System.out.println(sub(5, 3));
     }
 
     static int add(int a, int b) {
         return a + b;
+    }
+
+
+    static int sub(int a, int b) {
+        return a -b;
     }
 
 
