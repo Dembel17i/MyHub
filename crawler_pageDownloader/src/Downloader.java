f95e26cf9862a537c07d2fc8a65ee29cc0647a6d
Dembel17i <Dembel17i@gmail.com>
2017-07-31 19:26:21 +0300

    Download

--- crawler_pageDownloader/src/Downloader.java
+++ crawler_pageDownloader/src/Downloader.java
@@ -1,3 +1,5 @@
+package components;
+
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -7,30 +9,42 @@ import java.util.regex.Pattern;
 public class Downloader {
 
 
-    public String download(String url) throws UnsupportedEncodingException {
-        return getPage(url, getCharset(url));
+    public String download(String url) {
+        return loadPage(url, charsetLookUp(url));
     }
 
 
+    public String download(String url, String charsetName) {
+        return loadPage(url, charsetName);
+    }
 
-    private String getCharset(String url) {
+
+    private String charsetLookUp(String url) {
         Pattern p = Pattern.compile("charset=(\"|)[A-z,-]{1,}[0-9]{1,}");
-        Matcher m = p.matcher(getPage(url, "UTF-8"));
+        Matcher m = p.matcher(loadPage(url, "UTF-8"));
         String result = null;
-        while (m.find()) {
-            result = m.group().substring(8);
-        }
-        if (result.startsWith("\"")) {
-            result = result.replaceAll("\"", "");
+        try {
+            while (m.find()) {
+                result = m.group().substring(8);
+            }
+            if (result.startsWith("\"")) {
+                result = result.replaceAll("\"", "");
+            }
+        } catch (NullPointerException e) {
+            try {
+                throw new RuntimeException("Не удолось определить кодировку страницы. Будет установлена кодировка по умолчанию - UTF-8!");
+            } catch (RuntimeException e1) {
+                e1.printStackTrace();
+                return "utf-8";
+            }
         }
         return result;
     }
 
 
-
-    private String getPage(String url, String charset) {
+    private String loadPage(String url, String charset) {
         BufferedReader br = null;
-        URL page = null;
+        URL page;
         String line;
         StringBuilder sb = new StringBuilder();
 
@@ -42,7 +56,17 @@ public class Downloader {
                 sb.append(line);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                throw new RuntimeException("Не удалось загрузить страницу!");
+            } catch (RuntimeException e1) {
+                e1.printStackTrace();
+            }
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return sb.toString();
     }
